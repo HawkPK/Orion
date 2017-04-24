0dd82582cd251133dd3d36b9c3a6b403076250dc
Hawk84 <paweljastrzabek@o2.pl>
2017-04-24 11:11:26 +0200

    #5 Add New Unit Testing

---
+++ Orion.Test/TestType/RefVsValueTypeTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orion.Test.TestType
+{
+    [TestClass]
+    public class RefVsValueTypeTest { 
+
+    [TestMethod]
+        public void DoesItComputeCorrectly()
+        {
+            int c1 = 10;
+            int c2 = c1;
+
+            Assert.AreEqual(c1, c2);
+
+            c1 = 100;
+
+            Assert.AreNotEqual(c1, c2);
+        }
+    }
+}
